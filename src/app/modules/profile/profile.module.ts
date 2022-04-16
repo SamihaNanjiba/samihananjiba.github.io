51b6aa062ace084e4310a5e131447c4cc15eaa69
Samiha <snanjiba@gmail.com>
2022-04-16 14:16:11 +0200

    Added Intro, Education and Work Components to Profile Component

--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ProfileComponent } from 'src/app/components/profile/profile.component';
 import { ProfileRoutingModule } from './profile-routing.module';
+import { IntroductionComponent } from 'src/app/components/profile/introduction/introduction.component';
+import { EducationComponent } from 'src/app/components/profile/education/education.component';
+import { WorkComponent } from 'src/app/components/profile/work/work.component';
 
 @NgModule({
-  declarations: [ProfileComponent],
+  declarations: [
+    ProfileComponent,
+    IntroductionComponent,
+    EducationComponent,
+    WorkComponent,
+  ],
   imports: [CommonModule, ProfileRoutingModule],
 })
 export class ProfileModule {}
