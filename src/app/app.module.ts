91ec9397418b308a107fe569561fe7a3743e6214
Samiha <snanjiba@gmail.com>
2022-04-18 16:11:43 +0200

    Updated style and content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+// import { HeaderComponent } from './components/header/header.component';
+// import { FooterComponent } from './components/footer/footer.component';
 import { ProfileModule } from './modules/profile/profile.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  // declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, ProfileModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -6,13 +6,17 @@ import { ProfileRoutingModule } from './profile-routing.module';
 import { IntroductionComponent } from 'src/app/components/profile/introduction/introduction.component';
 import { EducationComponent } from 'src/app/components/profile/education/education.component';
 import { WorkComponent } from 'src/app/components/profile/work/work.component';
+import { HeaderComponent } from 'src/app/components/header/header.component';
+import { FooterComponent } from 'src/app/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     ProfileComponent,
+    HeaderComponent,
     IntroductionComponent,
     EducationComponent,
     WorkComponent,
+    FooterComponent,
   ],
   imports: [CommonModule, ProfileRoutingModule],
 })
